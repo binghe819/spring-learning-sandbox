5dde6a0e601e0b0b761f1f42e906ae7b7773c2ab
binghe819 <binghe819@naver.com>
2021-06-02 15:26:07 +0900

    chore: 그레이들 및 MockMvc 초기 설정

--- src/main/java/com/binghe/HelloController.java
+++ src/main/java/com/binghe/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello " + "binghe";
+        return "Hello";
     }
 
 }
---
+++ src/test/java/com/binghe/HelloControllerTest.java
@@ -0,0 +1,48 @@
+package com.binghe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = WebConfig.class)
+class HelloControllerTest {
+
+    @Autowired
+    private HelloController helloController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloController).build();
+    }
+
+    @Test
+    void dependency() {
+        assertThat(helloController).isNotNull();
+        assertThat(mockMvc).isNotNull();
+    }
+
+    @DisplayName("@GetMapping - GET 요청을 처리할 수 있다.")
+    @Test
+    void helloTest_get() throws Exception {
+        mockMvc.perform(get("/hello"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello"));
+    }
+}
