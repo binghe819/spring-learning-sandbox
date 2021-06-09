ef52ced1bfe10258e8a227f590dd4732992952cb
binghe819 <binghe819@naver.com>
2021-06-09 23:52:29 +0900

    test(method argument): RequestParam 테스트 추가

---
+++ src/main/java/com/binghe/method_arguments/RequestParamController.java
@@ -0,0 +1,33 @@
+package com.binghe.method_arguments;
+
+import com.binghe.domain.Event;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @제목 : @RequestParam
+ *
+ * @내용 : 요청 매개변수(쿼리 매개변수)와 폼 데이터를 메서드 아규먼트로 받아올 수 있다.
+ */
+@RestController
+public class RequestParamController {
+
+    @GetMapping("/test/requestparam")
+    public Event requestParam(@RequestParam(value = "id") Integer id, @RequestParam(value = "name") String name) {
+        return new Event(id, name);
+    }
+
+    @GetMapping("/test/requestparam/default")
+    public Event requestParamDefault(@RequestParam(required = false, defaultValue = "0") Integer id) {
+        return new Event(id, "default");
+    }
+
+    @GetMapping("/test/requestparam/map")
+    public Event requestParamByMap(@RequestParam Map<String, String> param) {
+        Integer id = Integer.parseInt(param.get("id"));
+        String name = param.get("name");
+        return new Event(id, name);
+    }
+}
---
+++ src/test/java/com/binghe/method_arguments/RequestParamControllerTest.java
@@ -0,0 +1,85 @@
+package com.binghe.method_arguments;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.binghe.WebConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = WebConfig.class)
+class RequestParamControllerTest {
+
+    @Autowired
+    private RequestParamController requestParamController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(requestParamController).build();
+    }
+
+    @Test
+    void dependency() {
+        assertThat(requestParamController).isNotNull();
+        assertThat(mockMvc).isNotNull();
+    }
+
+    @DisplayName("@RequestParam(성공케이스) - 요청 매개변수에 들어있는 단순 타입 데이터를 메서드 아규먼트로 받아올 수 있다.")
+    @Test
+    void requestParam() throws Exception {
+        mockMvc.perform(get("/test/requestparam?id=1&name=binghe"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id").value("1"))
+            .andExpect(jsonPath("name").value("binghe"));
+    }
+
+    @DisplayName("@RequestParam(실패케이스) - 요청 매개변수를 하나라도 주지 않을 경우 400(MissingServletRequestParameterException)가 발생한다.")
+    @Test
+    void requestParamWithNone() throws Exception {
+        mockMvc.perform(get("/test/requestparam?id=1"))
+            .andDo(print())
+            .andExpect(status().is(400));
+    }
+
+    @DisplayName("@RequestParam(실패케이스) - 요청 매개변수의 타입이 다를 경우 400(MethodArgumentTypeMismatchException)가 발생한다.")
+    @Test
+    void requestParamWithWrongDataType() throws Exception {
+        mockMvc.perform(get("/test/requestparam?id=binghe"))
+            .andDo(print())
+            .andExpect(status().is(400));
+    }
+
+    @DisplayName("@RequestParam(기본값 설정) - 요청 매개변수의 defaultValue설정을 통해 기본값을 설정해줄 수 있다.")
+    @Test
+    void requestParamDefaultValue() throws Exception {
+        mockMvc.perform(get("/test/requestparam/default"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id").value("0"));
+    }
+
+    @DisplayName("@RequestParam(Map사용) - 요청 매개변수를 Map 형태로 받아올 수 있다. (빈 값일 경우 따로 예외처리를 해줘야하는 단점이 있다.)")
+    @Test
+    void requestParamWithMap() throws Exception {
+        mockMvc.perform(get("/test/requestparam/map?id=1&name=binghe"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id").value(1))
+            .andExpect(jsonPath("name").value("binghe"));
+    }
+}
