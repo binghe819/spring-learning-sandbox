0b9024c6049e317dfb3dd08de55d43ee2087cbd7
binghe819 <binghe819@naver.com>
2021-07-24 15:17:31 +0900

    feat: 스프링Core(부트없이) 놀이터 기본 세팅 (DispatcherServlet등록)

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# 스프링 놀이터
-> 스프링 부트가 아닌 스프링을 테스트하는 놀이터입니다.
+# 스프링 놀이터 - SpringCore 
+> 스프링부트없이 SpringCore를 실험하는 놀이터
 
-<br>
 
-## 놀이터
-- [스프링MVC](https://github.com/binghe819/spring-learning-sandbox/tree/mvc)
-- [스프링Core](https://github.com/binghe819/spring-learning-sandbox/tree/core)
-- [스프링 테스트](https://github.com/binghe819/spring-learning-sandbox/tree/test)
---
+++ src/main/java/com/binghe/WebConfig.java
@@ -0,0 +1,13 @@
+package com.binghe;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+}
